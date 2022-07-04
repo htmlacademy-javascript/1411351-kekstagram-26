c909245823d86c510bec8f3731a595800cec943d
Leman <niklaysm@yandex.ru>
2022-07-04 21:40:25 +0300

    выполняет домашнее задание 4.15

--- js/main.js
+++ js/main.js
@@ -1,30 +1,56 @@
-function getRandomNumbers (numberOne, numberTwo) {
-  if ((numberOne || numberTwo) < 0) {
-    return 'Выберите число больше нуля';
-  } else {
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
 
-    if (numberTwo <= numberOne) {
-      return 'Второе число должно быть больше первого';
-    } else {
+function getRandomPositiveInteger (a, b) {
 
-      if ((typeof numberOne || typeof numberTwo) !== 'number') {
-        return 'Вы ввели НЕ число';
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
 
-      } else {
-        const result = Math.round(Math.random() * (numberTwo - numberOne) + numberOne);
-        // Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-        return result;
-      }
-    }
-  }
-}
-getRandomNumbers (1, 3);
+  const result = Math.random() * (upper - lower + 1) + lower;
 
-function checkCommentString (commentString, maxLength) {
-  if (commentString.length < maxLength) {
-    return true;
-  }
-  return false;
+  return Math.floor(result);
 }
 
-checkCommentString ('Здесь находится комментарий к фотографии', 140);
+function checkStringLength (string, length) {
+  return string.length <= length;
+}
+
+const galery = [];
+const idPhoto = 25;
+const comments = [];
+const idComments = 6;
+const message = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+const subscribers = [
+  'Твитти',
+  'Честер',
+  'Мурка',
+  'Киса',
+  'Чикен',
+  'Пётр'
+];
+
+for (let i = 0; i <= idComments - 1; i++) {
+  comments[i] = {
+    'id': getRandomPositiveInteger(1, 6),
+    'avatar': 'img/avatar-' + getRandomPositiveInteger(1, 6) + '.svg',
+    'message': message[getRandomPositiveInteger(0, message.length - 1)],
+    'name': subscribers[getRandomPositiveInteger(0, subscribers.length - 1)]
+}
+}
+
+for (let i = 0; i <= idPhoto - 1; i++) {
+  galery[i] = {
+    'id': i + 1,
+    'url': 'photos/' + (i + 1) + '.jpg',
+    'description': 'Котик моет лапки',
+    'likes': getRandomPositiveInteger(15, 200),
+    'comments': comments[getRandomPositiveInteger(0, comments.length - 1)]
+}
+}
+
