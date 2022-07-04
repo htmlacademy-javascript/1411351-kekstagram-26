d5b978fb5ed650386c52aeb09ac310d7f5305fd0
Leman <niklaysm@yandex.ru>
2022-07-04 23:40:36 +0300

    выполняет домашнее задание 4.15_3

--- js/main.js
+++ js/main.js
@@ -38,18 +38,18 @@ const subscribers = [
 for (let i = 0; i <= idComments - 1; i++) {
   comments[i] = {
     'id': getRandomPositiveInteger(1, 6),
-    'avatar': 'img/avatar-' + String(getRandomPositiveInteger(1, 6)) + '.svg',
+    'avatar': `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
     'message': message[getRandomPositiveInteger(0, message.length - 1)],
     'name': subscribers[getRandomPositiveInteger(0, subscribers.length - 1)]
-  }
+  };
 }
 
 for (let i = 0; i <= idPhoto - 1; i++) {
   galery[i] = {
     'id': i + 1,
-    'url': 'photos/' + String(i + 1) + '.jpg',
+    'url': `photos/${i + 1}.jpg`,
     'description': 'Котик моет лапки',
     'likes': getRandomPositiveInteger(15, 200),
     'comments': comments[getRandomPositiveInteger(0, comments.length - 1)]
-  }
+  };
 }
