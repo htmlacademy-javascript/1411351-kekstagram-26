0275a5f27417f1bb61a635d21752f3d6e87dd83a
Leman <niklaysm@yandex.ru>
2022-07-05 22:49:33 +0300

    выполняет домашнее задание 4.15_4

--- js/main.js
+++ js/main.js
@@ -1,24 +1,11 @@
-// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
-
 function getRandomPositiveInteger (a, b) {
-
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-
   const result = Math.random() * (upper - lower + 1) + lower;
-
   return Math.floor(result);
 }
 
-// function checkStringLength (string, length) {
-//   return string.length <= length;
-// }
-
-const galery = [];
-const idPhoto = 25;
-const comments = [];
-const idComments = 6;
-const message = [
+const messages = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -34,22 +21,26 @@ const subscribers = [
   'Чикен',
   'Пётр'
 ];
+let commentId = 1;
 
-for (let i = 0; i <= idComments - 1; i++) {
-  comments[i] = {
-    'id': getRandomPositiveInteger(1, 6),
-    'avatar': `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
-    'message': message[getRandomPositiveInteger(0, message.length - 1)],
-    'name': subscribers[getRandomPositiveInteger(0, subscribers.length - 1)]
+const createComment = () => {
+  return {
+    id: commentId++,
+    avatar: `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
+    message: messages[getRandomPositiveInteger(0, messages.length - 1)],
+    name: subscribers[getRandomPositiveInteger(0, subscribers.length - 1)]
   };
 }
 
-for (let i = 0; i <= idPhoto - 1; i++) {
-  galery[i] = {
-    'id': i + 1,
-    'url': `photos/${i + 1}.jpg`,
-    'description': 'Котик моет лапки',
-    'likes': getRandomPositiveInteger(15, 200),
-    'comments': comments[getRandomPositiveInteger(0, comments.length - 1)]
+const createPhoto = (_, index) => {
+  return {
+    id: index + 1,
+    url: `photos/${index + 1}.jpg`,
+    description: 'Котик моет лапки',
+    likes: getRandomPositiveInteger(15, 200),
+    comments: Array.from({ length: getRandomPositiveInteger(0, 5) }, createComment)
   };
-}
+};
+
+const PHOTOS_COUNT = 25;
+const gallery = Array.from({ length: PHOTOS_COUNT }, createPhoto);
