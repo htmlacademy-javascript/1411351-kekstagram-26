8a83e01a016df665efab3d9f4596ac0cce12498f
Rail Sakhaviev <sahaviev@gmail.com>
2022-07-26 01:19:30 +0400

    add api functions, add messages modal for error and success data sending, connect application with server

--- js/util.js
+++ js/util.js
@@ -1,7 +1,25 @@
-function getRandomPositiveInteger (a, b) {
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-}
-export { getRandomPositiveInteger };
+const ERROR_MESSAGE_TIME = 3000;
+
+const showLoadErrorMessage = (message) => {
+  const errorMessageElement = document.createElement('div');
+  errorMessageElement.style.zIndex = '999';
+  errorMessageElement.style.position = 'absolute';
+  errorMessageElement.style.left = '50%';
+  errorMessageElement.style.transform = 'translate(-50%)';
+  errorMessageElement.style.top = '0';
+  errorMessageElement.style.padding = '10px';
+  errorMessageElement.style.fontSize = '26px';
+  errorMessageElement.style.lineHeight = '1.5';
+  errorMessageElement.style.textAlign = 'center';
+  errorMessageElement.style.backgroundColor = 'tomato';
+
+  errorMessageElement.textContent = message;
+
+  document.body.append(errorMessageElement);
+
+  setTimeout(() => {
+    errorMessageElement.remove();
+  }, ERROR_MESSAGE_TIME);
+};
+
+export { showLoadErrorMessage };
